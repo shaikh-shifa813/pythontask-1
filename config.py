d4a3c7b61cc090118a116c53043e0e36e7bb47ad
shaikh-shifa813 <shaikhruhinmoimran@gmail.com>
2026-01-05 16:39:08 +0530

    config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
+#Imports Flask framework and SQLAlchemy ORM
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+#Creates a Flask application
 app = Flask(__name__)
 
-# MySQL config (XAMPP defaults: user=root, password empty)
+#MySQL config (XAMPP defaults: user=root, password empty)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/activity_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+#links it to the Flask app.
 db = SQLAlchemy(app)
